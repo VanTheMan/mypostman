b2910c1505f18d557bb0ccb5ba1b4c6a02961fdf
Loan Dinh <loandt.uet@gmail.com>
2016-12-25 21:59:01 +0700

    change flow data from parent to child

--- components/App.js
+++ components/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import SubmitForm from './SubmitForm';
 import ResponsePreview from './ResponsePreview';
+const ipc = require('electron').ipcRenderer;
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {responseContent: ''};
+  }
+
+  componentDidMount(){
+      ipc.on('server-response', function(event, arg){
+          console.log('receive server response');
+          let content = JSON.stringify(arg, undefined, 4);
+          this.setState({responseContent: content});
+      }.bind(this));
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,7 +27,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <SubmitForm />
-        <ResponsePreview />
+        <ResponsePreview responseContent={this.state.responseContent}/>
       </div>
     );
   }
--- components/ResponsePreview.js
+++ components/ResponsePreview.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
-const ipc = require('electron').ipcRenderer;
 
 class ResponsePreview extends Component {
     constructor(props){
         super(props);
-        this.state = { responseContent: ''};
-    };
-
-    componentDidMount(){
-        ipc.on('server-response', function(event, arg){
-            console.log('receive server response');
-            let content = JSON.stringify(arg, undefined, 4);
-            this.setState({responseContent: content});
-        }.bind(this));
     };
 
     render() {
         return (
         <div className="ResponsePreview">
-            <pre className="response-content">{this.state.responseContent}</pre>
+            <pre className="response-content">{this.props.responseContent}</pre>
         </div>
         );
     }
